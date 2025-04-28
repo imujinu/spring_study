7a09007bdf0421496b3baa7be22590dc1c4167d6
imujinu <wlsdnrhdwkd@gmail.com>
2025-04-28 14:05:14 +0900

    save

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,22 +1,41 @@
 package hello.hello_spring.controller;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("key"," text입력");
+        model.addAttribute("key" , "hello");
         return "hello";
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
-        model.addAttribute("name",  name);
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
         return "hello-mvc";
     }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Data
+    static class Hello{
+        private String name;
+    }
 }
+
---
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,9 @@
+package hello.hello_spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
