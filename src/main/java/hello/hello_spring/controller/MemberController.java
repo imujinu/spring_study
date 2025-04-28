2f31c4bfe0def642fc672aff55cbcb1ee63d10d0
imujinu <wlsdnrhdwkd@gmail.com>
2025-04-28 16:27:38 +0900

    regist

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,9 +1,15 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+    MemberService memberService;
 
+    @Autowired
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
 }
